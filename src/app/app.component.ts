b9b2f21f25a103e7b6fb35ac1a2591253e3a7772
MrDony <i200513@nu.edu.pk>
2022-12-21 17:34:35 +0500

    front end done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'understandingComponents';
+  count: number=0;
+  counter(task:string)
+  {
+    if(task==='-')
+      this.count--;
+    else
+      this.count++;
+  }
+  ngOnInit()
+  {
+    console.log(this.title);
+
+  }
 }
---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent {
+  claculateNoPairs() 
+  {
+    throw new Error('Method not implemented.');
+    
+  }
+
+}
