44aa8dc60c8a391b3f0725200c1e21aedc9b26a3
MrDony <i200513@nu.edu.pk>
2022-12-22 11:04:45 +0500

    Logic made. need to make pretty

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -6,10 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-component.component.css']
 })
 export class MyComponentComponent {
-  claculateNoPairs() 
+
+  inputArray='Your Array = ';
+  outputValues='Output = ';
+  private splitText(by:string)
   {
-    throw new Error('Method not implemented.');
     
+  }
+  claculateNoPairs(array:string) 
+  {
+    this.inputArray='Your Array = ';
+    this.outputValues='Output = '
+    console.log(array);
+    //throw new Error('Method not implemented.');
+    this.inputArray+=(array);
+
+    let splitStrings:string[]=array.split(',');
+
+    let splitVals:number[]=[];
+    for(let i=0;i<splitStrings.length;i++)
+    {
+      splitVals.push(Number(splitStrings[i]));
+    }
+    console.log(splitStrings);
+    console.log(splitVals);
+
+    var outputString:string='';
+    for(var i=0;i<splitVals.length;i++)
+    {
+      if(splitVals[i]===-1)
+      {
+        continue;
+      }
+      for(var j=i+1;j<splitVals.length;j++)
+      {
+        if(splitVals[i]===splitVals[j])
+        {
+          splitVals[i]=-1;
+          splitVals[j]=-1;
+          break;
+        }
+      }
+    }
+    console.log(splitVals);
+
+    for(let i=0;i<splitVals.length;i++)
+    {
+      outputString+=' ['+i.toString()+']=';
+      if(splitVals[i]===-1)
+        outputString+='p';
+      else
+        outputString+=splitVals[i].toString();
+    
+    }
+
+    this.outputValues+=outputString;
+
   }
 
 }
